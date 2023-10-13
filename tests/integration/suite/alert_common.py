c6aa7503231b65dee2515190ee7a66bcf2d99d36
Don Johnson <dj@codetestcode.io>
2023-10-13 00:22:28 -0700

    refactored alert test

--- tests/integration/suite/alert_common.py
+++ tests/integration/suite/alert_common.py
@@ -1,29 +1,28 @@
+from flask import Flask, request
+from threading import Thread
 import requests
 
-from flask import request
-from threading import Thread
+# Constants
+DINGTALK_MESSAGE = "Dingtalk setting validated"
+MICROSOFTTEAMS_MESSAGE = "MicrosoftTeams setting validated"
 
 
 class MockServer(Thread):
     def __init__(self, port=5000):
         super().__init__()
-        from flask import Flask
         self.port = port
         self.app = Flask(__name__)
-        self.url = "http://127.0.0.1:%s" % self.port
-
+        self.url = f"http://127.0.0.1:{self.port}"
         self.app.add_url_rule("/shutdown", view_func=self._shutdown_server)
 
     def _shutdown_server(self):
-        from flask import request
         if 'werkzeug.server.shutdown' not in request.environ:
             raise RuntimeError('Not running the development server')
         request.environ['werkzeug.server.shutdown']()
         return 'Server shutting down...'
 
     def shutdown_server(self):
-        requests.get("http://127.0.0.1:%s/shutdown" % self.port,
-                     headers={'Connection': 'close'})
+        requests.get(f"http://127.0.0.1:{self.port}/shutdown", headers={'Connection': 'close'})
         self.join()
 
     def run(self):
@@ -31,7 +30,6 @@ class MockServer(Thread):
 
 
 class MockReceiveAlert(MockServer):
-
     def api_microsoft_teams(self):
         message = request.json.get("text")
         assert message == MICROSOFTTEAMS_MESSAGE
@@ -43,19 +41,9 @@ class MockReceiveAlert(MockServer):
         return '{"errcode":0,"errmsg":""}'
 
     def add_endpoints(self):
-        self.app.add_url_rule("/microsoftTeams",
-                              view_func=self.api_microsoft_teams,
-                              methods=('POST',))
-        self.app.add_url_rule("/dingtalk/<path:url>/",
-                              view_func=self.api_dingtalk,
-                              methods=('POST',))
-        pass
+        self.app.add_url_rule("/microsoftTeams", view_func=self.api_microsoft_teams, methods=('POST',))
+        self.app.add_url_rule("/dingtalk/<path:url>/", view_func=self.api_dingtalk, methods=('POST',))
 
     def __init__(self, port):
         super().__init__(port)
         self.add_endpoints()
-
-
-DINGTALK_MESSAGE = "Dingtalk setting validated"
-
-MICROSOFTTEAMS_MESSAGE = "MicrosoftTeams setting validated"
