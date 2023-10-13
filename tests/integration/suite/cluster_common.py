aa3a6867204d9b3a96da3bcf5d79cf80045901a9
Don Johnson <dj@codetestcode.io>
2023-10-13 00:27:04 -0700

    refactored cluster common code

--- tests/integration/suite/cluster_common.py
+++ tests/integration/suite/cluster_common.py
@@ -1,40 +1,30 @@
-import subprocess
 import os
 import re
+import subprocess
 import yaml
-from .common import random_str
 from jinja2 import Template
+from .common import random_str
+
+# Constants
+MAX_TRIES = 3
 
 
 def generate_cluster_config(request, dind_rke_node_num):
-    # generate a random and kube_config file
-    dind_name = 'dind-' + random_str()
-    dind_cluster_config_file = dind_name + '.yml'
-    dind_kube_config_file = 'kube_config_' + dind_name + '.yml'
-    cluster_config_tmpl = get_rke_config_template()
-    # generate nodes
-    random_nodes = [
-            'node-' +
-            random_str()
-            for x in range(dind_rke_node_num)]
-    rke_config_template = Template(cluster_config_tmpl)
-    rendered_tmpl = rke_config_template.render(
-        random_nodes=random_nodes)
-    # write config file on disk
-    cluster_config_file = open(dind_cluster_config_file, 'w')
-    cluster_config_file.write(rendered_tmpl)
-    cluster_config_file.close()
+    dind_name = f'dind-{random_str()}'
+    dind_cluster_config_file = f'{dind_name}.yml'
+    dind_kube_config_file = f'kube_config_{dind_name}.yml'
 
-    request.addfinalizer(lambda: cleanup_dind(
-        dind_cluster_config_file,
-        dind_name + '.rkestate'
-    ))
+    random_nodes = [f'node-{random_str()}' for _ in range(dind_rke_node_num)]
 
-    return \
-        dind_name, \
-        yaml.safe_load(rendered_tmpl), \
-        dind_cluster_config_file, \
-        dind_kube_config_file
+    rke_config_template = Template(get_rke_config_template())
+    rendered_tmpl = rke_config_template.render(random_nodes=random_nodes)
+
+    with open(dind_cluster_config_file, 'w') as cluster_config_file:
+        cluster_config_file.write(rendered_tmpl)
+
+    request.addfinalizer(lambda: cleanup_dind(dind_cluster_config_file, f'{dind_name}.rkestate'))
+
+    return dind_name, yaml.safe_load(rendered_tmpl), dind_cluster_config_file, dind_kube_config_file
 
 
 def cleanup_dind(cluster_file, state_file):
@@ -44,8 +34,7 @@ def cleanup_dind(cluster_file, state_file):
 
 
 def get_rke_config_template():
-    dind_cluster_config_j2 = """
----
+    return """---
 authentication:
     strategy: "x509|webhook"
 nodes:{% for node in random_nodes %}
@@ -56,33 +45,28 @@ nodes:{% for node in random_nodes %}
     - worker
     - etcd{% endfor %}
 """
-    return dind_cluster_config_j2
 
 
 def create_cluster(cluster_config_file):
-    raise Exception('cluster creation needs refactor')
-    # attempt to resolve unknown random rke up errors
-    for _ in range(3):
+    for _ in range(MAX_TRIES):
         try:
             return subprocess.check_output(
-                'rke up --dind --config ' +
-                cluster_config_file,
+                f'rke up --dind --config {cluster_config_file}',
                 stderr=subprocess.STDOUT, shell=True
             )
         except subprocess.CalledProcessError as err:
-            print('RKE up error: ' + str(err.output))
+            print(f'RKE up error: {err.output}')
     raise Exception('rke up failure')
 
 
 def remove_cluster(cluster_config_file):
     try:
         return subprocess.check_output(
-            'rke remove --force --dind --config ' +
-            cluster_config_file,
+            f'rke remove --force --dind --config {cluster_config_file}',
             stderr=subprocess.STDOUT, shell=True
         )
     except subprocess.CalledProcessError as err:
-        print('RKE down error: ' + str(err.output))
+        print(f'RKE down error: {err.output}')
         raise err
 
 
@@ -110,4 +94,5 @@ def import_cluster(admin_mc, kube_config_file, cluster_name):
     os_env = os.environ.copy()
     os_env['KUBECONFIG'] = kube_config_file
     subprocess.check_output(insecure_command, env=os_env, shell=True)
+
     return imported_cluster
